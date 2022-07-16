ec4034515c65888d87afdcd495a77ed54b00c846
Nobi9 <100874045+Nobi9@users.noreply.github.com>
2022-07-16 12:33:12 +0200

    refacto

--- starterOnly/src/js/modal.js
+++ starterOnly/src/js/modal.js
@@ -10,7 +10,6 @@ function editNav() {
 const form = document.querySelector('form');
 const formDatas = document.querySelectorAll(".formData"); 
 let inputs = document.querySelectorAll("input[type='text'], input[type='email'], input[type='date'], input[type='number'], input[type='radio'], input[type='checkbox']");
-const radiosInputs = document.querySelectorAll('input[type="radio"]')
 const submitBtn = document.querySelector(".btn-submit");
 const closeBtn = document.querySelector(".close");
 const modalBg = document.querySelector(".bground");
@@ -31,14 +30,12 @@ vSubBtn.addEventListener("click", shutVmodal);
 
 // ============================== Form Attributes ================================================= //
 formDatas.forEach(formData => {                                     
-  formData.querySelector('input').setAttribute('required','');
+  formData.querySelector('input').setAttribute('required','');        // Pose l'attribut required sur chaque première input du parent .formData
   formData.querySelector('input').setAttribute('autocomplete','true');
 })
-radiosInputs.forEach(radio => {
-  radio.setAttribute('required','');
-})
-document.getElementById('checkbox1').setAttribute('checked', '');    // pre-checked box
 
+document.getElementById('checkbox1').setAttribute('checked', '');    // pre-checked box
+document.getElementById('checkbox1').setAttribute('required','');
 // ============================== Err msg Data + Functions show/hide ============================== // 
 let errors = {
   first : 'Veuillez saisir votre prénom (2 caractères min)',
@@ -58,15 +55,14 @@ let regexArr = {
   number : "[0-9]{1,2}",
 }
 
-function radioIsChecked(radios){                            // nodList de radios en paramètre,
-  return (Object.keys(radios).map(key => {                  // Itération sur un tableau d'objets-clés,
-    return radios[key].checked})).some(x => x === true)     // Pour retourner une unique valeur true si au moins un radio est checked 
-} 
+let userData = [];
+let formValidity = [];
 
 function isValid(input){                                              // Détermine les actions à faire si une input est valide
   input.setCustomValidity('')                                         // passer le test de validité html5
   input.parentNode.setAttribute('data-error-visible', 'false');       // cacher l'erreur éventuelle
 }
+
 function isInvalid(input){                                              // Détermine les actions à faire si une input n'est pas valide
   input.setCustomValidity(errors[`${input.name}`]);                     // empêcher de passer le test de validité html5
   input.parentNode.setAttribute('data-error', `${input.validationMessage}`); // donner la data-err au parent
@@ -74,102 +70,63 @@ function isInvalid(input){                                              // Déte
 }
 
 function checkInputs(input){  // une input en param
-  
   switch (input.type){ 
 
     case 'text': case 'date': case 'email': case 'number':
-      const regexType = regexArr[`${input.type}`];          // Selectionne la regex adaptée à cette input dans le tableau des regex
       let regex = RegExp('');
-      
       (input.required) ?                                                  // input required ? 
-        (regex = RegExp(regexType)) : (regex = RegExp('^$|'+ regexType)); // True : regex en fonction du type d'input. False : regex idem mais avec la possibilité "empty
+        (regex = RegExp(regexArr[`${input.type}`])) : (regex = RegExp('^$|'+ regexArr[`${input.type}`])); // True : regex en fonction du type d'input. False : regex idem mais avec la possibilité "empty
         
       (regex.test(input.value)) ?                                         // L'input match la regex ? 
-        (isValid(input)) : (isInvalid(input))     
+        (isValid(input)) : (isInvalid(input));                            // show / hide errMsg
+        
+      userData.push(input.value);                 // stocke la valeur
+      formValidity.push(regex.test(input.value))  // stocke la validité
       break;
 
-    case 'radio' : 
-      if(input.required){                                                 // Radio required ? 
-        if(radioIsChecked(document.querySelectorAll("[type='radio']"))){  // un radio parmis les radio est checké ? 
-          isValid(input)
-        } else {                                                          // aucun radio checked ? 
-          isInvalid(input)
-        }
-      } else {                                                             // If not required ?
-        isValid(input)
-      }
+    case 'radio':
+      let siblings = Array.from(input.parentNode.querySelectorAll('input'));  // retourn la nodList du parent de l'input 
+      let oneIsChecked = siblings.find(e => e.checked);
+      let oneIsRequired = siblings.find(e => e.required);
+
+      ((oneIsChecked && oneIsRequired) || !oneIsRequired) ? 
+        (formValidity.push(true))                                       // Un check + required ou pas required ? TRUE
+        :
+        (formValidity.push(false));                                     // 
+
+      (oneIsChecked) ?                                         // Pas check et required : 'null' + false
+        (userData.push(input.parentNode.querySelector(':checked').value))
+        : 
+        (userData.push('empty'));
       break;
 
     case 'checkbox' : 
-      if (input.required) {             // checkbox required ? 
-        if(input.checked){                // checked ?  
-          isValid(input)
-        } else {                          // If not ?
-          isInvalid(input)
-        }                                     
-      } else {                         // checkbox not required ? 
-        isValid(input)
-      }
+      if((input.required && input.checked) || !input.required) {   // (radio required + checked) OU not required ? 
+        isValid(input);
+        formValidity.push(true);
+       } else {
+        isInvalid(input);
+        formValidity.push(false);
+       }
+      userData.push(input.checked);
       break;
   }
-};
+}
 
-submitBtn.addEventListener('click', () => {  // ajouter keypress ? 
+submitBtn.addEventListener('click', () => {  // ajouter keypress ?
+    userData = [];                           // Vide les tableaux avant de les remplir
+    formValidity = [];
     inputs.forEach((input) => {
-      checkInputs(input)
-    })
-})
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  e.stopPropagation();
-  getFormData(form);
-  userDatas.push([getFormData(form)]);
-  console.log(userDatas)
-  shutModal();                                                                          // ..
-  launchVmodal(); 
-})
-
-// ======================================================================================================================== //
-// ======================================================================================================================== //
-// =========================================== !!!!! OPTIONAL !!!!!======================================================== //
-// ======================================================================================================================== //
-// ======================================================================================================================== //
-
-
-// ======================================================================================================================== //
-// ============================================= Data Function ============================================================ //
-// ======================================================================================================================== //
-
-const userDatas = [];
-
-const getFormData = form => {
-  if (!form || form.tagName !== "FORM") return;                   // Test : si !form, fin de la fonction. 
-
-  const { elements } = form;                  
-  const data = {};                                                // object of the data we are going to retrieve                
-  const allowedTags = ["INPUT", "TEXTAREA", "SELECT"];            // the HTML tags we want to retrieve
-  const excludedInputTypes = ["button", "reset", "submit"];       // the types of inputs that we dont consider
-
-  [].slice                                  // [].slice.call() will convert elements of type HTMLFormControlsCollection to an array
-    .call(elements)                        // we return the tag only if it is contained in `allowedTags` and if its name has a value
-    .filter(node =>allowedTags.indexOf(node.tagName) !== -1 && node.name)
-    .forEach(node => {                                               
-      if (node.tagName === "INPUT" && excludedInputTypes.indexOf(node.type) === -1      // in the case of an input
-      ) {
-        if ((node.type !== "radio" && node.type !== "checkbox") ||(node.type === "radio" && node.checked)) { // Si ni radio ni checkbox ou radio.checked
-          data[node.name] = node.value;                                                                      // data = value
-        }
-        if (node.type === "checkbox") {                                                                       // si checkbx : data = checked
-          data[node.name] = node.checked;
-        }
-      }
-      // in the case of a textarea
-      /*if (node.tagName === "TEXTAREA") {data[node.name] = node.value;}    // in the case of a select with options
-      if (node.tagName === "SELECT") {
-        const hasOption = node.options.length && node.options[node.selectedIndex];
-        data[node.name] = hasOption ? node.options[node.selectedIndex].value : "";
-      }*/
+      checkInputs(input)                     // Boucle sur toutes les input, pour valider le form et stocker les datas
     });
-  return data;
-};
+    userData.splice(5,5);                    // Supprime les data en trop généré par le forEach() au niveau des radio 
+    return console.log(userData);
+})
+
+form.addEventListener('submit', e => {
+      e.preventDefault(); // this will stop the event from further propagation and the submission will not be executed
+      e.stopPropagation(); //not always necessary
+      console.log(userData)
+      shutModal();                                                                          // ..
+      launchVmodal();     
+});
